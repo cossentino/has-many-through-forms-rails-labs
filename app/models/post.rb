533345f4b19d8e095246b809554848ec5ddc42a3
Ian Cossentino <icossentino@gmail.com>
2021-01-20 22:31:40 -0500

    commit tree 3006f717c5c72639336e2ca1d01626cc6e515a6c

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,5 +4,17 @@ class Post < ActiveRecord::Base
   has_many :comments
   has_many :users, through: :comments
 
+  def categories_attributes=(categories_attributes)
+    categories_attributes.each do |i, cat_atts|
+      if cat_atts[:name].present?
+        category = Category.find_or_create_by(name: cat_atts[:name])
+        if !self.categories.include?(category)
+          self.post_categories.build(category_id: category.id)
+        end
+      end
+    end
+  end
+
+
 
 end
